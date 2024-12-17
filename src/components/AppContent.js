80d9f2625a205f908445f23121951012c67db57a
Bufigol <f.vasquez.tort@gmail.com>
2024-12-17 13:34:05 -0300

    primera version de componente consumo electrico

--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import HomePage from './HomePage';
-import DashboardElectrico from './DashboardElectrico'; // Corrected import name
+import DashboardElectrico from './DashboardElectrico'; 
+import ConsumoElectrico from './ConsumoElectrico';
 
 function AppContent() {
     const [userPermissions, setUserPermissions] = useState([]);
@@ -12,6 +13,7 @@ function AppContent() {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/dashboard-electrico" element={<DashboardElectrico />} />
+                <Route path="/consumo-electrico" element={<ConsumoElectrico />} />
                 <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
         </div>
