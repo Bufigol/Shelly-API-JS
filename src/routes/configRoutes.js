ef87b99573d0f6870dbb9a9c81af59e8d941e725
Bufigol <f.vasquez.tort@gmail.com>
2024-12-26 11:05:04 -0300

    comienzo de integración de endpoints de configuración.

--- src/routes/configRoutes.js
+++ src/routes/configRoutes.js
@@ -1,19 +1,24 @@
 // src/routes/configRoutes.js
 const express = require('express');
 const router = express.Router();
-const configController = require('../controllers/configController');
+const sem_configController = require('../controllers/sem_configController');
+const tel_configController = require('../controllers/tel_configController');
 const { authMiddleware } = require('../middlewares');
 
 // Get current system parameters
 router.get('/parameters', 
     authMiddleware.authenticate,
-    configController.getSystemParameters.bind(configController)
+    sem_configController.getSystemParameters.bind(sem_ConfigController)
 );
 
 // Update system parameters
 router.post('/parameters',
     authMiddleware.authenticate,
-    configController.updateSystemParameters.bind(configController)
+    sem_configController.updateSystemParameters.bind(sem_ConfigController)
 );
 
+router.get('/teltonica',
+    authMiddleware.authenticate,
+    tel_configController.getSystemParameters
+);
 module.exports = router;
