b481f51b114b93cf61ece7de89e9777a64172ebe
Bufigol <f.vasquez.tort@gmail.com>
2024-12-26 13:03:42 -0300

    rutas de configuracion

--- src/routes/configRoutes.js
+++ src/routes/configRoutes.js
@@ -6,19 +6,50 @@ const tel_configController = require('../controllers/tel_configController');
 const { authMiddleware } = require('../middlewares');
 
 // Get current system parameters
-router.get('/parameters', 
+router.get('/sem/parametros', 
     authMiddleware.authenticate,
     sem_configController.getSystemParameters.bind(sem_ConfigController)
 );
 
 // Update system parameters
-router.post('/parameters',
+router.post('/sem/parametros',
     authMiddleware.authenticate,
     sem_configController.updateSystemParameters.bind(sem_ConfigController)
 );
 
-router.get('/teltonica',
+router.get('/teltonica/parametros',
     authMiddleware.authenticate,
     tel_configController.getSystemParameters
 );
+
+router.post('/teltonica/parametros',
+    authMiddleware.authenticate,
+    tel_configController.updateSystemParameters
+);
+
+router.get('/teltonica/configuracion_beacon/:beacon_id',
+    authMiddleware.authenticate,
+    tel_configController.configurarBeacon
+);
+
+router.get('/teltonica/temperatura-umbrales',
+    authMiddleware.authenticate,
+    tel_configController.getConfigTemperaturaUmbral
+);
+
+router.post('/teltonica/temperatura-umbrales',
+    authMiddleware.authenticate,
+    tel_configController.setConfigTemperaturaUmbral
+);
+
+router.get('/teltonica/umbrales',
+    authMiddleware.authenticate,
+    tel_configController.getConfigUmbrales
+);
+
+router.post('/teltonica/umbrales',
+    authMiddleware.authenticate,
+    tel_configController.setConfigUmbrales
+);
+
 module.exports = router;
