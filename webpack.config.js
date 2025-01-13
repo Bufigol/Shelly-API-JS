a95ad0cce042c113c15257483d4198cc8066f658
Bufigol <f.vasquez.tort@gmail.com>
2025-01-13 10:46:55 -0300

    inicio testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 // webpack.config.js
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/'
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
   },
   module: {
     rules: [
@@ -14,37 +14,48 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'], // Agrega esta regla para manejar CSS
+        use: ["style-loader", "css-loader"], // Agrega esta regla para manejar CSS
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: 'images',
-              publicPath: 'dist/images'
+              name: "[name].[ext]",
+              outputPath: "images",
+              publicPath: "dist/images",
             },
           },
         ],
       },
     ],
   },
-  resolve: {
-    extensions: ['.js'],
-    fallback: {
-      "buffer": require.resolve("buffer/"),
-      "crypto": require.resolve("crypto-browserify"),
-      "stream": require.resolve("stream-browserify"),
-      "util": require.resolve("util/"),
-      "vm": require.resolve("vm-browserify")
-    }
+  devServer: {
+    port: 8080, // Cambiamos a 8080 en lugar de 3030
+    proxy: [
+      {
+        context: ["/api"],
+        target: "http://localhost:3030",
+      },
+    ],
+    historyApiFallback: true,
+    hot: true,
   },
-  mode: 'development', // o 'production'
+  resolve: {
+    extensions: [".js"],
+    fallback: {
+      buffer: require.resolve("buffer/"),
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      util: require.resolve("util/"),
+      vm: require.resolve("vm-browserify"),
+    },
+  },
+  mode: "development", // o 'production'
 };
